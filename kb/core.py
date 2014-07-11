4d4fa0b50028624f0f79e574280e83a79a68d442
Yan Zhang <cyanogenoid@cyanogenoid.com>
2014-07-11 16:45:02 +0200

    Add some comments to core.py

--- kb/core.py
+++ kb/core.py
@@ -11,6 +11,11 @@ class Keyboard(metaclass=abc.ABCMeta):
 
     @abc.abstractproperty
     def keys(self):
+        """ Return the keys of this keyboard.
+
+        :returns: An iterable of keys
+
+        """
         pass
 
 
@@ -20,4 +25,15 @@ class Actor(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def evaluate(self, keyboard, layout, corpus):
+        """ Calculate how much effort the actor has to exert to type the corpus using the layout.
+
+        :param keyboard: The keyboard the layout is applied to.
+        :type keyboard: Keyboard
+        :param layout: The layout function to be evaluated.
+        :type layout: dict
+        :param corpus: The corpus used for evaluation.
+        :type corpus: iterable of strings
+        :returns: A floating point value in range [0, +inf] where lower values indicate less effort
+
+        """
         pass
