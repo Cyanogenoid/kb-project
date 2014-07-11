ae00365dfe5cd47fe903dcbd14fc19b901961882
Yan Zhang <cyanogenoid@cyanogenoid.com>
2014-07-11 16:06:42 +0200

    Implement Keyboard class

--- kb/core.py
+++ kb/core.py
@@ -1,4 +1,14 @@
+import abc
 from collections import namedtuple
 
 
 Key = namedtuple('Key', ['x', 'y'])
+
+
+class Keyboard(metaclass=abc.ABCMeta):
+    def __init__(self):
+        pass
+
+    @abc.abstractproperty
+    def keys(self):
+        pass
