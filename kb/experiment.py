322e2f8ae4f62f1392b850c5c4e70734f4a9256a
Yan Zhang <cyanogenoid@cyanogenoid.com>
2014-09-03 02:20:30 +0100

    Full experiment with SingleFinger actor on reduced Core33 keyboard

--- kb/experiment.py
+++ kb/experiment.py
@@ -27,8 +27,9 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 alphabet_set = set(alphabet)
 
+print('Processing corpus...')
 text = itertools.chain.from_iterable(map(str.lower, brown.words()))
-corpus = collections.Counter(n_wise(filter(lambda c: c in alphabet_set, text), 2))
+corpus = collections.Counter(n_wise(filter(lambda c: c in alphabet_set, text), 3))
 
 
 ### DEAP Stuff ###
@@ -42,19 +43,38 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", lambda l: (ac.evaluate(kb, l, alphabet, corpus),))
 toolbox.register("mate", tools.cxPartialyMatched)
 toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.1)
-toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("select", tools.selTournament, tournsize=6)
+
+
+def show_individual(individual):
+    font_size = 14
+
+    max_x = max(keys[i].x for i in individual)
+    max_y = max(keys[i].y for i in individual)
+
+    from PIL import Image, ImageDraw, ImageFont
+    image = Image.new('RGBA', (max_x + 2*font_size, max_y + 2*font_size))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("DejaVuSans.ttf", font_size)
+
+    for i, c in enumerate(alphabet):
+        key = keys[individual[i]]
+        target_position = (key.x, key.y)
+        draw.text(target_position, c, fill=(0, 0, 0), font=font)
+
+    image.save('layout.png')
 
 
 def main():
     import numpy
-    pop = toolbox.population(n=50)
+    pop = toolbox.population(n=100)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
-    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=10, stats=stats, halloffame=hof, verbose=True)
+    pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=200, stats=stats, halloffame=hof, verbose=True)
 
     return pop, logbook, hof
 
@@ -68,6 +88,7 @@ if __name__ == "__main__":
     plt.plot(gen, min_, label="minimum")
     plt.plot(gen, max_, label="maximum")
     plt.xlabel("Generation")
-    plt.ylabel("Fitness")
-    plt.legend(loc="lower right")
+    plt.ylabel("Effort")
+    plt.legend(loc="upper right")
     plt.show()
+    show_individual(hof[0])
